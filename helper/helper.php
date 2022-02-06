d1ef0f73ed496c8844633ebb65c5b77822d7a881
Hadi Majid <hadimajid@outlook.com>
2022-02-06 16:38:25 +0500

    email helper

--- helper/helper.php
+++ helper/helper.php
@@ -96,7 +96,6 @@ function sendEmail($subject, $to, $file_path, $msg) {
     catch(Exception $e) {
         //wh_log('email.php Sent Email failed to customer ' . $to . ' 1/2');
         //wh_log("Mailer Error: {$mail->ErrorInfo} 2/2");
-        echo $mail->ErrorInfo ;
         return false;
     }
 }
